8ddf8ba7f1143a9febe2d43fa6d63ca1a4cde89a
dimasik <228@mail.ru>
2023-06-27 15:49:32 +0500

    nofing

--- main.py
+++ main.py
@@ -17,12 +17,13 @@
 #         print(sym, counter)
 # M * N или N * N если все символы разные
 
-def strcounter(s):
-    syms_county = {}
-    for sym in s:
-        syms_county[sym] = syms_county.get(sym, 0) + 1
+# def strcounter(s):
+#     syms_county = {}
+#     for sym in s:
+#         syms_county[sym] = syms_county.get(sym, 0) + 1
+#
+#     print(syms_county)
+#
+# s = 'aaabc'
+# strcounter(s) # O(N) = N
 
-    print(syms_county)
-
-s = 'aaabc'
-strcounter(s) # O(N) = N
